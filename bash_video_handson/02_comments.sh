c61427adb6ff3f84a8f52049650adc44a99e0cc6
Ahmad Shehanshah <ahmadshehansha@gmail.com>
2024-02-14 14:53:03 +0500

    Removed Feb 13 work

--- bash_video_handson/02_comments.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-echo "Checking Comments"
-
-#This is single comment
-
-<< This is how we write multi line comment>>
--- bash_video_handson/06_key-value.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-declare -A myArray
-
-myArray=( [name]=Ahmad [age]=20 [city]=Paris )
-
-echo "My name is ${myArray[name]}, age is ${myArray[age]}, and city is ${myArray[city]}"
-
