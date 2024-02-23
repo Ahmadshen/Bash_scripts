bcee0bc77f6b75be128286fd9864c2fc86bafc18
Ahmad Shehanshah <ahmadshehansha@gmail.com>
2024-02-23 12:20:17 +0500

    Updated Rotational Cipher

--- Rotational Cipher.sh
+++ Rotational Cipher.sh
@@ -3,10 +3,11 @@
 rot_cipher() {
     local str="$1"
     local rot="$2"
-    local lower=$(echo {a..z} | tr -d ' ')
-    local upper=$(echo {A..Z} | tr -d ' ')
-    local rotated_lower=$(echo "$lower$lower" | cut -c$((rot%26+1))-$((rot%26+26)))
-    local rotated_upper=$(echo "$upper$upper" | cut -c$((rot%26+1))-$((rot%26+26)))
+    local lower upper rotated_lower rotated_upper
+    lower=$(echo {a..z} | tr -d ' ')
+    upper=$(echo {A..Z} | tr -d ' ')
+    rotated_lower=$(echo "$lower$lower" | cut -c$((rot%26+1))-$((rot%26+26)))
+    rotated_upper=$(echo "$upper$upper" | cut -c$((rot%26+1))-$((rot%26+26)))
     echo "$str" | tr 'a-zA-Z' "$rotated_lower$rotated_upper"
 }
 
